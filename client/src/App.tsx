8dd53ed71ff61c5cc0d8d0d9ee6738efd7a2b752
Alex Martishin <asmartishin@gmail.com>
2024-09-29 11:41:52 +0200

    Add footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,36 @@
-import './App.css'
+import "./App.css"
 import Camera from "./components/Camera.tsx"
 
 function App() {
-  return <Camera />
+  return (
+    <>
+      <Camera />
+      <footer
+        style={{
+          textAlign: "left",
+          marginTop: "500px",
+          color: "#888",
+        }}
+      >
+        <p>
+          YOLOv8 WASM demo built by{" "}
+          <a href="https://www.martishin.com" target="_blank" style={{ color: "#888" }}>
+            Alex Martishin
+          </a>
+        </p>
+        <p>
+          View the source code on{" "}
+          <a
+            href="https://github.com/martishin/react-rust-wasm-yolo-object-detection"
+            target="_blank"
+            style={{ color: "#888" }}
+          >
+            GitHub
+          </a>
+        </p>
+      </footer>
+    </>
+  )
 }
 
 export default App
